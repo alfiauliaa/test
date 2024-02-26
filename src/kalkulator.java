0ffdd9cd8402f6d7a9260357894e4a78c5296ad0
alfiauliaa <alfiauliaazzahra@gmail.com>
2024-02-26 08:24:29 +0700

    first commit

---
+++ src/kalkulator.java
@@ -0,0 +1,13 @@
+public class kalkulator {
+    public static void main(String[] args) {
+
+        int a = 5 - 2;
+        System.out.println("Hasil pengurangan: " + a);
+
+        int b = 1 * 3;
+        System.out.println("Hasil perkalian: " + b);
+
+        int c = 1 + 2;
+        System.out.println("Hasil penambahan: " + c);
+    }
+}
